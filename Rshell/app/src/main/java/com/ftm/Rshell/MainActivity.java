364d89f5b5fe1cfbeb9e16bbf034f81ea06a4244
Chal13W1zz <freetechmods@gmail.com>
2021-06-23 21:06:48 +0300

    working local shell

--- Rshell/app/src/main/java/com/ftm/Rshell/MainActivity.java
+++ Rshell/app/src/main/java/com/ftm/Rshell/MainActivity.java
@@ -2,29 +2,39 @@ package com.ftm.Rshell;
 
 import android.app.*;
 import android.os.*;
-import android.view.*;
-import android.widget.*;
+	import android.util.Log;
+	import android.view.View;
+	import android.widget.Button;
+	import android.widget.EditText;
+	import android.widget.TextView;
+	import com.ftm.Rshell.ShellExecuter;
+	public class MainActivity extends Activity {
 
-public class MainActivity extends Activity 
-{
-	EditText getCmd;
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-		getCmd = (EditText) findViewById(R.id.enterCmd);
-    }
-	public void execute(View v){
-		//String cmd = getCmd.getText().toString();
-		try{
-		 int res = Runtime.getRuntime().exec("whoami").waitFor();
-		 getCmd.setText(res);
+		EditText input;
+		Button btn;
+		TextView out;
+		String command;
+
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.main);
+			input = (EditText)findViewById(R.id.inP);
+			btn = (Button)findViewById(R.id.run);
+			out = (TextView)findViewById(R.id.outp);
+			btn.setOnClickListener(new View.OnClickListener() {
+
+					@Override
+					public void onClick(View arg0) {
+						ShellExecuter exe = new ShellExecuter();
+						command = input.getText().toString();
+
+						String outp = exe.Executer(command);
+						out.setText(outp);
+						Log.d("Output", outp);
+					}
+				});
+
 		}
-		catch(Exception e){
-			e.printStackTrace();
-			return;
-		}
-		
+
 	}
-}
